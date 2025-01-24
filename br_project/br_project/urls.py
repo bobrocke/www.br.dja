256490ebb8f0a54104ccd17ae75e5064ab2b9490
Bob Rockefeller <bob@bobrockefeller.com>
2025-01-24 18:55:07 -0500

    Fixing the app

--- br_project/br_project/urls.py
+++ br_project/br_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for br project.
+URL configuration for br_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("colors/", include("colors.urls")),
-    path('admin/', admin.site.urls),
+  path("colors/", include("colors.urls")),
+  path('admin/', admin.site.urls),
 ]
