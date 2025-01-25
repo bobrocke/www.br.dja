cac1c213e6949aee80a44ce8cc53e1ac795559ef
Bob Rockefeller <bob@bobrockefeller.com>
2025-01-24 19:40:50 -0500

    Got a Template working

--- br_project/colors/views.py
+++ br_project/colors/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+  template = loader.get_template("colors/index.html")
+  return HttpResponse(template.render({}, request))
